1f3cf5c9567cc6abb8f3dde009648aa7efe52e88
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-05-22 21:27:31 -0500

    feat: Add query for sample rate and buffer size
    Request from AudioManager in MainActivity and Log.  Set to 44.1k and 512 in case of null

--- app/src/main/java/xyz/peast/beep/MainActivity.java
+++ app/src/main/java/xyz/peast/beep/MainActivity.java
@@ -1,15 +1,21 @@
 package xyz.peast.beep;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Get the sample rate and buffer size, if possible from the device
+        // Set to 44.1k samples/s and 512 frame buffer
+        // A frame = sample size * channels (stereo with 16bit samples = 32 bits (4 bytes)/frame)
+        String samplerateString = null;
+        String buffersizeString = null;
+        if (Build.VERSION.SDK_INT >= 17) {
+            AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+            samplerateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            buffersizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        }
+        if (samplerateString == null) {
+            samplerateString = "44100";
+        }
+        if (buffersizeString == null) {
+            buffersizeString = "512";
+        }
+        Log.d(TAG, "sampleRateString: " + samplerateString);
+        Log.d(TAG, "buffersizeString: " + buffersizeString);
+
     }
 
     @Override
