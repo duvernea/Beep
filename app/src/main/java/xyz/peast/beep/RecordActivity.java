71015411001736a711b3be2ed1b2989757f018f6
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-07-05 10:01:45 -0500

    feat: Implement basic admob banner in RecordActivity

--- app/src/main/java/xyz/peast/beep/RecordActivity.java
+++ app/src/main/java/xyz/peast/beep/RecordActivity.java
@@ -3,11 +3,23 @@ package xyz.peast.beep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class RecordActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        mAdView = (AdView) findViewById(R.id.adview);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("839737069995AAD5519D71B8B267924D")
+                .build();
+        mAdView.loadAd(adRequest);
     }
 }
