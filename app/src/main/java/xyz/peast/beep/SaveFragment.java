cd08bf2ae7c021775762831d42b4ebfd38cf4af4
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-07-30 20:21:13 -0500

    feat: Add spinner in SaveFragment for Board selection

--- app/src/main/java/xyz/peast/beep/SaveFragment.java
+++ app/src/main/java/xyz/peast/beep/SaveFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by duvernea on 7/30/16.
@@ -17,6 +19,8 @@ public class SaveFragment extends Fragment {
 
     private Context mContext;
 
+    private Spinner mBoardSpinner;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
@@ -25,6 +29,10 @@ public class SaveFragment extends Fragment {
 
         mContext = getActivity();
 
+        mBoardSpinner = (Spinner) rootView.findViewById(R.id.board_name_spinner);
+        String[] spinnerItems = new String[] {"myBeeps", "Sweetie", "Mom & Dad", "Work Crewz"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, spinnerItems);
+        mBoardSpinner.setAdapter(adapter);
         return rootView;
     }
 }
