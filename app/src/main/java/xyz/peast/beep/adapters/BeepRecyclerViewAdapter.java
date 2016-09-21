11a68fee6cc549c6b56a5d45a9753e6e202cc3de
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-09-21 18:57:15 -0500

    feat: Add Google Analytics lib, Remove Picasso lib

--- app/src/main/java/xyz/peast/beep/adapters/BeepRecyclerViewAdapter.java
+++ app/src/main/java/xyz/peast/beep/adapters/BeepRecyclerViewAdapter.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
 
 import xyz.peast.beep.Constants;
 import xyz.peast.beep.MainActivity;
