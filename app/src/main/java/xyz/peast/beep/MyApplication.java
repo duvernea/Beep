91519baf7b99803cb02fc2c27f1a1b5550c441b1
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-09-21 22:46:31 -0500

    fix: Fix bug in startTracking()

--- app/src/main/java/xyz/peast/beep/MyApplication.java
+++ app/src/main/java/xyz/peast/beep/MyApplication.java
@@ -1,6 +1,7 @@
 package xyz.peast.beep;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -10,17 +11,19 @@ import com.google.android.gms.analytics.Tracker;
  */
 public class MyApplication extends Application {
 
+    private static final String TAG = MyApplication.class.getSimpleName();
+
     public Tracker mTracker;
 
     // Start analytics tracking
     public void startTracking() {
+        Log.d(TAG, "startTracking called");
 
-        if (mTracker != null) {
+        if (mTracker == null) {
+            Log.d(TAG, "Enabling auto tracking");
             GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
             mTracker = ga.newTracker(R.xml.track_app);
             ga.enableAutoActivityReports(this);
-
-
         }
     }
     public Tracker getTracker() {
