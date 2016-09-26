a672a5c279ba0ae1e5fd97fcc6f2a076861bff62
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-09-25 22:26:54 -0500

    feat: Add widget to Manifest and create WidgetProvider stub

---
+++ app/src/main/java/xyz/peast/beep/widget/WidgetProvider.java
@@ -0,0 +1,15 @@
+package xyz.peast.beep.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by duverneay on 9/25/16.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
