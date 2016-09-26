f887aa3d4302861f02cccb1d4a11c43d5caa686e
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-09-25 22:54:49 -0500

    feat: Add pending intent to widget to open MainActivity

--- app/src/main/java/xyz/peast/beep/widget/WidgetProvider.java
+++ app/src/main/java/xyz/peast/beep/widget/WidgetProvider.java
@@ -1,8 +1,14 @@
 package xyz.peast.beep.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import xyz.peast.beep.MainActivity;
+import xyz.peast.beep.R;
 
 /**
  * Created by duverneay on 9/25/16.
@@ -11,5 +17,24 @@ public class WidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.temp_textview, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 }
