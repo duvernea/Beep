8c8f21c60c1810dd5a963cca8c4f1590378d63ef
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-10-17 15:22:11 -0500

    feat: Add IntentService for Widget

--- app/src/main/java/xyz/peast/beep/widget/WidgetProvider.java
+++ app/src/main/java/xyz/peast/beep/widget/WidgetProvider.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import xyz.peast.beep.MainActivity;
@@ -14,27 +15,16 @@ import xyz.peast.beep.R;
  * Created by duverneay on 9/25/16.
  */
 public class WidgetProvider extends AppWidgetProvider {
+
+    private static final String TAG = WidgetProvider.class.getSimpleName();
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-        final int N = appWidgetIds.length;
+        context.startService(new Intent(context, WidgetIntentService.class));
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];
+        Log.d(TAG, "onUpdate, WidgetProvider");
 
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            views.setOnClickPendingIntent(R.id.temp_textview, pendingIntent);
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
     }
 }
