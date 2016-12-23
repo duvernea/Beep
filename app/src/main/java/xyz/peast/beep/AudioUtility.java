b26e23366327b7c5e6d963980acb08d656d89c5d
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-22 20:09:37 -0600

    debug: Add header to temp wav file
    Note: hear a very similar 'noise' in this wav file that I'm hearing in the mp3 file.  Perhaps the noise has entered prior to any mp3 encoding.

--- app/src/main/java/xyz/peast/beep/AudioUtility.java
+++ app/src/main/java/xyz/peast/beep/AudioUtility.java
@@ -55,10 +55,72 @@ public class AudioUtility {
         try {
             outputStream = new BufferedOutputStream(new FileOutputStream(output), OUTPUT_STREAM_BUFFER);
             outputStream_test = new BufferedOutputStream(new FileOutputStream(output_test), OUTPUT_STREAM_BUFFER_test);
+
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
         }
+        try {
+            byte[] header = new byte[44];
+            int totalDataLen = 400000;
+            int totalAudioLen = 400000;
+            int channels = 2;
+            int longSampleRate = 44100;
+            int bitsPerSample = 16;
+            int byteRate = longSampleRate * channels * bitsPerSample / 8;
+
+            header[0] = 'R';  // RIFF/WAVE header
+            header[1] = 'I';
+            header[2] = 'F';
+            header[3] = 'F';
+            header[4] = (byte) (totalDataLen & 0xff);
+            header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+            header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+            header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+            header[8] = 'W';
+            header[9] = 'A';
+            header[10] = 'V';
+            header[11] = 'E';
+            header[12] = 'f';  // 'fmt ' chunk
+            header[13] = 'm';
+            header[14] = 't';
+            header[15] = ' ';
+            header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+            header[17] = 0;
+            header[18] = 0;
+            header[19] = 0;
+            header[20] = 1;  // format = 1
+            header[21] = 0;
+            header[22] = (byte) channels;
+            header[23] = 0;
+            header[24] = (byte) (longSampleRate & 0xff);
+            header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+            header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+            header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+            header[28] = (byte) (byteRate & 0xff);
+            header[29] = (byte) ((byteRate >> 8) & 0xff);
+            header[30] = (byte) ((byteRate >> 16) & 0xff);
+            header[31] = (byte) ((byteRate >> 24) & 0xff);
+            header[32] = (byte) (2 * 16 / 8);  // block align
+            header[33] = 0;
+            header[34] = (byte) bitsPerSample;  // bits per sample
+            header[35] = 0;
+            header[36] = 'd';
+            header[37] = 'a';
+            header[38] = 't';
+            header[39] = 'a';
+            header[40] = (byte) (totalAudioLen & 0xff);
+            header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+            header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+            header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+            outputStream_test.write(header, 0, 44);
+        } catch (IOException ioe) {
+            Log.d(TAG, "Couldn't write header");
+        }
+
         int bytesRead = 0;
 
         short[] buffer_l = new short[CHUNK_SIZE];
@@ -75,6 +137,7 @@ public class AudioUtility {
                     bytesRead = waveReader.read(buffer_l, buffer_r, CHUNK_SIZE);
                     Log.d(TAG, "bytes read: " + bytesRead);
                     /*** TROUBLESHOOTING ********/
+                if (bytesRead == 8192) {
                     int bytesRead_temp = bytesRead * 2;
                     ByteBuffer buffer = ByteBuffer.allocate(bytesRead_temp);
                     buffer.order(ByteOrder.LITTLE_ENDIAN);
@@ -83,7 +146,7 @@ public class AudioUtility {
                     int shortbufferlength = bytes.length;
                     outputStream_test.write(bytes, 0, bytesRead_temp);
                     /*** TROUBLESHOOTING ********/
-
+                }
                     if (bytesRead > 0) {
                         Log.d(TAG, "bytes read from wav: " + bytesRead);
 
