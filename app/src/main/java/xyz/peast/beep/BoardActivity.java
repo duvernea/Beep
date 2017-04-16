56dfbcaf0bf9feb9e0cef188fdf64688820a4c33
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-16 11:23:47 -0500

    feat: Add up navigation to BoardActivity
    New problem introduced - the logo/app name is now smaller than previously. May need support library 23.1 for easy scaling of this to fill height, but maintain aspect ratio

--- app/src/main/java/xyz/peast/beep/BoardActivity.java
+++ app/src/main/java/xyz/peast/beep/BoardActivity.java
@@ -20,6 +20,7 @@ import android.os.Message;
 import android.os.Messenger;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -105,6 +106,11 @@ public class BoardActivity extends AppCompatActivity implements LoaderManager.Lo
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setDisplayShowTitleEnabled(false);
+        }
 
         mBoardImage = (ImageView) findViewById(R.id.board_imageview);
         mImageSavedBroadcastReceiver = new BroadcastReceiver() {
